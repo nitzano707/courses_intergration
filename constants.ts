2f122639bba7a04fbc907a5c7d81081de7b121b0
nitzano707 <nitzan.orot@gmail.com>
2025-10-27 13:50:17 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,5 +3,5 @@
 // Make sure the first row of your Google Sheet contains the exact English headers as defined in the Course type.
 // e.g., CourseID,CourseName,LecturerName,SemesterPrimary,SemesterSecondary,CourseFormat,RationaleAbstract,CourseGoals,AssessmentMethods
 // export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1IW4J7g729wsLlbeTmghObUYSoqT6sdCC5-XUkskX_Zc/export?format=csv&gid=0';
-export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1jSROElRVDaHO8In0Goab6xJ46jWgI-BRS3ZRAymeXtg/export?format=csv&gid=0';
+export const GOOGLE_SHEET_CSV_URL = 'export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1jSROElRVDaHO8In0Goab6xJ46jWgI-BRS3ZRAymeXtg/export?format=csv&gid=1091734925';';
 
