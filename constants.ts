7a826736374abbc661c91f52cc40725c4346c07f
nitzano707 <nitzan.orot@gmail.com>
2025-10-27 13:45:40 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,4 +2,6 @@
 // This is the public URL to the Google Sheet, exported as a CSV file.
 // Make sure the first row of your Google Sheet contains the exact English headers as defined in the Course type.
 // e.g., CourseID,CourseName,LecturerName,SemesterPrimary,SemesterSecondary,CourseFormat,RationaleAbstract,CourseGoals,AssessmentMethods
-export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1jSROElRVDaHO8In0Goab6xJ46jWgI-BRS3ZRAymeXtg/edit?usp=sharing';
+// export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1IW4J7g729wsLlbeTmghObUYSoqT6sdCC5-XUkskX_Zc/export?format=csv&gid=0';
+export const GOOGLE_SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/1jSROElRVDaHO8In0Goab6xJ46jWgI-BRS3ZRAymeXtg/export?format=csv&gid=0';
+
